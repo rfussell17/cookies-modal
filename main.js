cb94e5ae7863af5e00b751c4866e24265d2682cf
Robin Fussell <rfussell17@gmail.com>
2020-10-10 14:41:14 -0700

    mopdal working as expected

---
+++ main.js
@@ -0,0 +1,10 @@
+// var modal =
+// var overlay =
+var close = document.getElementById("closeBtn").addEventListener("click", function(){
+  document.getElementById("modal").style.display = "none";
+  document.getElementById("overlay").style.display = "block";
+})
+var accept = document.getElementById("acceptBtn").addEventListener("click", function(){
+  document.getElementById("modal").style.display = "none";
+  document.getElementById("overlay").style.display = "block";
+})
