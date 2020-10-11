5bfe81daf03f9d3625234d0b233d7146ca862a1c
Robin Fussell <rfussell17@gmail.com>
2020-10-11 15:11:56 -0700

    additional func. search bar, product menu

--- main.js
+++ main.js
@@ -13,3 +13,9 @@ var accept = document
     document.getElementById("modal").style.display = "none";
     document.getElementById("overlay").style.display = "block";
   });
+
+  document.getElementById("usernameInput").addEventListener("keyup", function(event) {
+    let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+    console.log(allNamesDOMCollection[0].textContent);
+});
