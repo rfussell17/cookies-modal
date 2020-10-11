3f81246b89465166a6eba5663f152c1f2480bf4b
Robin Fussell <rfussell17@gmail.com>
2020-10-11 13:45:58 -0700

    changed background color, formatted code on all files

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-var modal = document.getElementById("modal")
+var modal = document.getElementById("modal");
 
-var close = document.getElementById("closeBtn").addEventListener("click", function(){
-  modal.style.display = "none";
-  document.getElementById("overlay").style.display = "block";
-})
+var close = document
+  .getElementById("closeBtn")
+  .addEventListener("click", function () {
+    modal.style.display = "none";
+    document.getElementById("overlay").style.display = "block";
+  });
 
-var accept = document.getElementById("acceptBtn").addEventListener("click", function(){
-  document.getElementById("modal").style.display = "none";
-  document.getElementById("overlay").style.display = "block";
-})
+var accept = document
+  .getElementById("acceptBtn")
+  .addEventListener("click", function () {
+    document.getElementById("modal").style.display = "none";
+    document.getElementById("overlay").style.display = "block";
+  });
