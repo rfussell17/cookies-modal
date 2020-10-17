d5541936d57d839b83d81cc1b81771866a67606d
Robin Fussell <rfussell17@gmail.com>
2020-10-17 11:51:52 -0700

    carousel added, show bike model button added to trigger second modal

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 var modal = document.getElementById("modal");
+var carousel = document.getElementById("modal2");
+
+carousel.style.display = "none";
 
 var close = document
   .getElementById("closeBtn")
@@ -10,9 +13,19 @@ var close = document
 var accept = document
   .getElementById("acceptBtn")
   .addEventListener("click", function () {
-    document.getElementById("modal").style.display = "none";
+    modal.style.display = "none";
     document.getElementById("overlay").style.display = "block";
   });
+
+  var exit = document.getElementById("close").addEventListener('click', function(){
+    carousel.style.display = "none";
+    showModels.style.display = "block"
+  })
+
+  var showModels = document.getElementById("show").addEventListener('click', function(){
+    carousel.style.display = "block";
+    showModels.style.display = "none";
+    })
 
   document.getElementById("usernameInput").addEventListener("keyup", function(event) {
     let searchQuery = event.target.value.toLowerCase();
@@ -29,3 +42,42 @@ var accept = document
   }
 
 });
+
+
+const slides = document.getElementsByClassName('carousel-item');
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
+document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
+
+function moveToNextSlide() {
+    hideAllSlides();
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
+    showModels.style.background = "none";
+}
+
+function moveToPrevSlide() {
+  hideAllSlides();
+  if(slidePosition === 0){
+      slidePosition = slides.length -1;
+  } else {
+      slidePosition--;
+  }
+  slides[slidePosition].classList.add("carousel-item-visible");
+  showModels.style.display = "none";
+}
+
+function hideAllSlides() {
+    for(let slide of slides){
+        slides[slidePosition].classList.remove("carousel-item-visible");
+        slides[slidePosition].classList.add("carousel-item-hidden");
+        
+    }
+}
