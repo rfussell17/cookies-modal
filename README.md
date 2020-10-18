baa1a2ac6d4fb827ec38427cb3f06b36c1858d10
Robin Fussell <rfussell17@gmail.com>
2020-10-18 12:15:45 -0700

    features complete, README edited, code formatted

--- README.md
+++ README.md
@@ -1,18 +1,50 @@
 ## Cookies Modal
 
+
+
+## Summary
+
+This is a simple site I created to replicate the standard cookies pop-up that you get on sites that use cookies for user experience, a search box that can filter result displayed, as well as a second modal with a image slider.
+
+<br>
+
+ The cookies modal appears on page load and will close on button press. 
+
 <br>
 
  <img src="images/thumb.png">
 
 <br>
 
-<img src="images/thumb2.png"   title="hotel">
+<img src="images/thumb2.png">
 
 <br>
 
-## Summary
+After the first modal has been closed, the user can see the bike models in the top corner. The search box can be used to refine the list. If one of the models doesn't contain any of the characters typed, they will be removed from the list. 
 
-This is a simple modal I created to replicate the standard cookies pop-up that you get on sites that use cookies for user experience. It appears on page load and the modal will close on button press. This is one of a few modal related mini projects that I've been using to get practice in between portfolio projects to keep sharp with the basics.
+
+<br>
+
+<img src="images/thumb4.png" >
+
+<br>
+
+The user can also view the bike models via the "2020 models" button in the center of the page, this appears after the cookies modal has been exited. The button triggers a second modal that lets the user cycle through images. This modal can also be closed by button click.
+
+<br>
+
+ <img src="images/thumb5.png">
+
+<br>
+
+<img src="images/thumb6.png" >
+
+<br>
+
+
+<br>
+
+This is one of a few modal related mini projects that I've been using to get practice in between portfolio projects to keep sharp with the basics.
 
 
 **Author**
--- main.js
+++ main.js
@@ -2,12 +2,14 @@ var modal = document.getElementById("modal");
 var carousel = document.getElementById("modal2");
 
 carousel.style.display = "none";
+show.style.display = "none";
 
 var close = document
   .getElementById("closeBtn")
   .addEventListener("click", function () {
     modal.style.display = "none";
     document.getElementById("overlay").style.display = "block";
+    show.style.display = "block";
   });
 
 var accept = document
@@ -15,69 +17,77 @@ var accept = document
   .addEventListener("click", function () {
     modal.style.display = "none";
     document.getElementById("overlay").style.display = "block";
+    show.style.display = "block";
   });
 
-  var exit = document.getElementById("close").addEventListener('click', function(){
+var exit = document
+  .getElementById("close")
+  .addEventListener("click", function () {
     carousel.style.display = "none";
-    showModels.style.display = "block"
-  })
+    showModels.style.display = "block";
+  });
 
-  var showModels = document.getElementById("show").addEventListener('click', function(){
+var showModels = document
+  .getElementById("show")
+  .addEventListener("click", function () {
     carousel.style.display = "block";
     showModels.style.display = "none";
-    })
+  });
 
-  document.getElementById("usernameInput").addEventListener("keyup", function(event) {
+document
+  .getElementById("usernameInput")
+  .addEventListener("keyup", function (event) {
     let searchQuery = event.target.value.toLowerCase();
-    let bikeModels = document.getElementsByClassName('name');
-    
-    for(let i = 0; i < bikeModels.length; i++) {
+    let bikeModels = document.getElementsByClassName("name");
+
+    for (let i = 0; i < bikeModels.length; i++) {
       const currentName = bikeModels[i].textContent.toLowerCase();
-  
+
       if (currentName.includes(searchQuery)) {
         bikeModels[i].style.display = "inline-block";
-    } else {
+      } else {
         bikeModels[i].style.display = "none";
+      }
     }
-  }
+  });
 
-});
-
-
-const slides = document.getElementsByClassName('carousel-item');
+const slides = document.getElementsByClassName("carousel-item");
 let slidePosition = 0;
 const totalSlides = slides.length;
 
-document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
-document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
+document
+  .getElementById("carousel-button-next")
+  .addEventListener("click", moveToNextSlide);
+document
+  .getElementById("carousel-button-prev")
+  .addEventListener("click", moveToPrevSlide);
 
 function moveToNextSlide() {
-    hideAllSlides();
-    if (slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
-    }
-    
-    slides[slidePosition].classList.add("carousel-item-visible");
-    showModels.style.background = "none";
+  hideAllSlides();
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+
+  slides[slidePosition].classList.add("carousel-item-visible");
+  showModels.style.background = "none";
 }
 
 function moveToPrevSlide() {
   hideAllSlides();
-  if(slidePosition === 0){
-      slidePosition = slides.length -1;
+  if (slidePosition === 0) {
+    slidePosition = slides.length - 1;
   } else {
-      slidePosition--;
+    slidePosition--;
   }
   slides[slidePosition].classList.add("carousel-item-visible");
   showModels.style.display = "none";
 }
 
 function hideAllSlides() {
-    for(let slide of slides){
-        slides[slidePosition].classList.remove("carousel-item-visible");
-        slides[slidePosition].classList.add("carousel-item-hidden");
-        
-    }
+  for (let slide of slides) {
+    slides[slidePosition].classList.remove("carousel-item-visible");
+    slides[slidePosition].classList.add("carousel-item-hidden");
+  }
 }
